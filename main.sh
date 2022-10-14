a16cf842f737dafdc66eb35a4097921b732a17b0
chayawooth <chayawooth.puy@gmail.com>
2022-10-14 09:39:50 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,3 @@
+curl --location --request POST 'https://notify-api.line.me/api/notify' \
+--header 'Authorization: Bearer X3voaRa9RybWtYMU5p0caZJzzXq1qXRYZZq2ZzXHUVT' \
+--form 'message="Hi This is First Noti"'
